7fd8ff2b60f34442b7f6b63ca07c497592a0ec89
Jannette <jannette.krezel@gmail.com>
2022-02-19 00:06:43 +1100

    Added notice and alert messages for login

--- plant-game-site/app/controllers/plant_controller.rb
+++ plant-game-site/app/controllers/plant_controller.rb
@@ -1,8 +1,8 @@
 class PlantController < ApplicationController
   
-  
-  
   def index
+    flash[:notice] = "Login successful"
+    flash[:alert] = "Invalid email or password"
   end
 
   def dashboard
